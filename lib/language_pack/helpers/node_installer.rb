7f5eb4d83dac58496132a271bc62c518f04da52f
Pablo Baltodano <palinair@hotmail.com>
2022-08-06 11:57:31 -0600

    Fetching Node.js from a non deprecated URL

--- lib/language_pack/helpers/node_installer.rb
+++ lib/language_pack/helpers/node_installer.rb
@@ -5,7 +5,7 @@ class LanguagePack::NodeInstaller
   LEGACY_NODE_VERSION = "0.4.7"
   LEGACY_BINARY_PATH = "node-#{LEGACY_NODE_VERSION}"
 
-  NODEJS_BASE_URL     = "http://s3pository.heroku.com/node/v#{MODERN_NODE_VERSION}/"
+  NODEJS_BASE_URL     = "https://heroku-nodebin.s3.us-east-1.amazonaws.com/node/release/linux-x64/"
 
   def initialize(stack)
     @fetchers = {
